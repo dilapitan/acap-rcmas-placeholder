60f434e8a51c356550d3659d73373b580c06526b
dilapitan <domlapitan@gmail.com>
2024-01-17 23:00:17 +0800

    Add dummy data for ten day

--- src/data.js
+++ src/data.js
@@ -1,34 +1,75 @@
-// const DRY_SPELL_DATA1 = [
-//   {
-//     condition: 'b_normal',
-//     mo: 'oct',
-//     year: 2023,
-//   },
-//   {
-//     condition: 'wb_normal',
-//     mo: 'nov',
-//     year: 2023,
-//   },
-//   {
-//     condition: 'wb_normal',
-//     mo: 'dec',
-//     year: 2023,
-//   },
-//   {
-//     condition: 'b_normal',
-//     mo: 'jan',
-//   },
-//   {
-//     condition: 'wb_normal',
-//     mo: 'feb',
-//     year: 2024,
-//   },
-//   {
-//     condition: 'b_normal',
-//     mo: 'mar',
-//     year: 2024,
-//   },
-// ]
+const TENDAY_DUMMY_DATA = {
+  'Iriga City': [
+    {
+      rainfall: 'LIGHT RAINS',
+      day_str: '2024/01/15',
+      day_format: 'Mon Jan 15',
+      day: 1,
+    },
+    {
+      rainfall: 'LIGHT RAINS',
+      day_str: '2024/01/16',
+      day_format: 'Mon Jan 16',
+      day: 2,
+    },
+    {
+      rainfall: 'LIGHT RAINS',
+      day_str: '2024/01/17',
+      day_format: 'Mon Jan 17',
+      day: 3,
+    },
+    {
+      rainfall: 'LIGHT RAINS',
+      day_str: '2024/01/18',
+      day_format: 'Mon Jan 18',
+      day: 4,
+    },
+    {
+      rainfall: 'LIGHT RAINS',
+      day_str: '2024/01/19',
+      day_format: 'Mon Jan 19',
+      day: 5,
+    },
+    {
+      rainfall: 'LIGHT RAINS',
+      day_str: '2024/01/20',
+      day_format: 'Mon Jan 20',
+      day: 6,
+    },
+    {
+      rainfall: 'LIGHT RAINS',
+      day_str: '2024/01/21',
+      day_format: 'Mon Jan 21',
+      day: 7,
+    },
+    {
+      rainfall: 'LIGHT RAINS',
+      day_str: '2024/01/22',
+      day_format: 'Mon Jan 22',
+      day: 8,
+    },
+    {
+      rainfall: 'LIGHT RAINS',
+      day_str: '2024/01/23',
+      day_format: 'Mon Jan 23',
+      day: 9,
+    },
+    {
+      rainfall: 'LIGHT RAINS',
+      day_str: '2024/01/24',
+      day_format: 'Mon Jan 24',
+      day: 10,
+    },
+  ],
+  Caramoan: [
+    {
+      rainfall: 'LIGHT RAINS',
+      day_str: '2024/01/15',
+      day_format: 'Mon Jan 15',
+      day: 1,
+    },
+  ],
+}
 
 const SEASONAL_DUMMY_DATA = [
   {
@@ -64,5 +105,6 @@ const SEASONAL_DUMMY_DATA = [
 ]
 
 module.exports = {
+  TENDAY_DUMMY_DATA,
   SEASONAL_DUMMY_DATA,
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
 
-const { SEASONAL_DUMMY_DATA } = require('./data')
+const { TENDAY_DUMMY_DATA, SEASONAL_DUMMY_DATA } = require('./data')
 
 app.get('/', (req, res) => {
   res.send('Welcome to ACAP-RCMAS Placeholder!')
+})
+
+app.get('/tenday-dummy-data', (req, res) => {
+  res.status(200).json({
+    municipalities: TENDAY_DUMMY_DATA,
+  })
 })
 
 app.get('/seasonal-dummy-data', (req, res) => {
